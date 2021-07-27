1dcb53c47d5fc578cd73282e177ae224ebc0a9f7
Snapper <LearningWithSnapper@gmail.com>
2021-07-27 12:50:34 -0400

    Refactored UserSingleton into shared

---
+++ frontend/Shared/UserSingleton.cs
@@ -0,0 +1,26 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+
+namespace frontend.Shared
+{
+    public sealed class UserSingleton
+    {
+        private UserSingleton(){
+
+        } 
+
+        private static UserSingleton m_Instance = null;
+
+        public static UserSingleton Instance()
+        {
+            if(m_Instance == null)
+            {
+                m_Instance = new UserSingleton();
+            }
+
+            return m_Instance;
+
+        }
+
+        public JwtSecurityToken jwt { get; set; }
+    }
+}
